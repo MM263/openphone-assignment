29f92f8eb0e0742e410d8838755c7e389984850d
mm263 <tony@mm263.space>
2025-04-09 20:22:43 -0700

    Remove unnecessary old stuff

--- src/features/phoneNumbers/components/Test.tsx
+++
@@ -1,9 +0,0 @@
-import { useListPhoneNumbers } from "../hooks/useListPhoneNumbers";
-
-export const Test = () => {
-  const { data } = useListPhoneNumbers();
-
-  console.log({ data });
-
-  return <div>hi, test</div>;
-};
